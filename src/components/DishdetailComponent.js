424bf491844bd90f8340ad608549ca7c8635d19d
Balkı <balki1985@gmail.com>
2020-08-18 15:53:52 +0300

    Fetch

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -17,6 +17,7 @@ import {
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from "../shared/baseUrl";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -117,7 +118,7 @@ class CommentForm extends Component {
 function RenderDish({dish}) {
     return (
         <Card>
-        <CardImg width="100%" src={dish.image} alt={dish.name}/>
+        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
         <CardBody>
             <CardTitle>
                 {dish.name}

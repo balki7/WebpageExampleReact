3601b343a7793499e9a858925ebc608c70083841
Balkı <balki1985@gmail.com>
2020-08-18 11:44:03 +0300

    Thunk

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import DishDetail from "./DishdetailComponent";
 import About from './AboutComponent';
 import { connect } from 'react-redux';
 import { addComment, fetchDishes} from "../redux/ActionCreators";
+import { actions } from "react-redux-form";
 
 const mapStateToProps = state => {
     return {
@@ -22,7 +23,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => ({
     addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-    fetchDishes: () => {dispatch(fetchDishes())}
+    fetchDishes: () => {dispatch(fetchDishes())},
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 });
 
 class Main extends Component {
@@ -65,7 +67,7 @@ class Main extends Component {
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes.dishes} />} />
                     <Route path="/menu/:dishId" component={DishWithId} />
-                    <Route path="/contactus" component={Contact} />
+                    <Route path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
                     <Route path="/aboutus" component={AboutPage} />
                     <Redirect to="/home" />
                 </Switch>

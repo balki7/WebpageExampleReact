267df1b05c8c822040d147cf01ce2d23fba7f101
Balkı <balki1985@gmail.com>
2020-08-18 17:45:37 +0300

    Animations

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -18,6 +18,7 @@ import {Control, LocalForm, Errors} from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -117,17 +118,22 @@ class CommentForm extends Component {
 
 function RenderDish({dish}) {
     return (
-        <Card>
-        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
-        <CardBody>
-            <CardTitle>
-                {dish.name}
-            </CardTitle>
-            <CardText>
-                {dish.description}
-            </CardText>
-        </CardBody>
-    </Card>
+        <FadeTransform in
+                       transformProps={{
+                           exitTransform: 'scale(0.5) translateY(-50%)'
+                       }}>
+                <Card>
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
+                <CardBody>
+                    <CardTitle>
+                        {dish.name}
+                    </CardTitle>
+                    <CardText>
+                        {dish.description}
+                    </CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
     );
 }
 
@@ -136,11 +142,15 @@ function RenderComments({comments, postComment, dishId}) {
         if(comment != null) {
             return (
                 <div className="container" key={comment.id}>
-                    <li key={comment.id} className="mt-3 mb-3">
-                        {comment.comment}
-                        <br/>
-                        -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                    </li>
+                    <Stagger in>
+                        <Fade in>
+                            <li key={comment.id} className="mt-3 mb-3">
+                                {comment.comment}
+                                <br/>
+                                -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                            </li>
+                        </Fade>
+                    </Stagger>
                 </div>
             );
         }
